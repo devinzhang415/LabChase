43229ac406870d43a7a87f046cdf31b59e854e0a
LilG00s3 <ekimmel8@gatech.edu>
2024-08-09 16:02:04 -0400

    rotation complete

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -7,21 +7,29 @@ public class Path : MonoBehaviour
 {
     public Transform[] Points;
     public InputActionReference toggleReference;
+    public InputActionReference rotateReference;
     public GameObject path;
+    public GameObject pathParent;
 
     [SerializeField]
     private float moveSpeed;
-
+    [SerializeField]
+    private float rotationSpeed = 30f;
     private int pointsIndex;
     private bool toggleState = true;
+    private bool isRotating = false;
     // Start is called before the first frame update
     void Start()
     {
+        
         pointsIndex = 0;
         transform.position = Points[pointsIndex].transform.position;
 
+        isRotating = false;
         toggleState = true;
+
         toggleReference.action.started += TogglePathMesh;
+        rotateReference.action.started += RotatePath;
     }
 
     // Update is called once per frame
@@ -41,10 +49,18 @@ public class Path : MonoBehaviour
                 pointsIndex = 0;
             }
         }
+
+        if (isRotating)
+        {
+            float rotationAmount = rotationSpeed * Time.deltaTime;
+            pathParent.transform.Rotate(Vector3.up, rotationAmount);
+        }
+
     }
     private void OnDestroy()
     {
         toggleReference.action.started -= TogglePathMesh;
+        rotateReference.action.performed -= RotatePath;
     }
 
     private void TogglePathMesh(InputAction.CallbackContext context)
@@ -59,5 +75,11 @@ public class Path : MonoBehaviour
             path.SetActive(true);
             toggleState = !toggleState;
         }
+    }
+
+    private void RotatePath(InputAction.CallbackContext context)
+    {
+        isRotating = !isRotating;
+
     }
 }
