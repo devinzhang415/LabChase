71e283cccf4f928f8ad977a3e23bd276f63519ba
LilG0053 <ekimmel8@gatech.edu>
2024-08-11 22:05:45 -0400

    debug log
    -How to export vals?

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -1,85 +1,99 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Path : MonoBehaviour
+namespace UnityEngine
 {
-    public Transform[] Points;
-    public InputActionReference toggleReference;
-    public InputActionReference rotateReference;
-    public GameObject path;
-    public GameObject pathParent;
 
-    [SerializeField]
-    private float moveSpeed;
-    [SerializeField]
-    private float rotationSpeed = 30f;
-    private int pointsIndex;
-    private bool toggleState = true;
-    private bool isRotating = false;
-    // Start is called before the first frame update
-    void Start()
+
+    public class Path : MonoBehaviour
     {
-        
-        pointsIndex = 0;
-        transform.position = Points[pointsIndex].transform.position;
+        public Transform[] Points;
+        public InputActionReference toggleReference;
+        public InputActionReference rotateReference;
+        public GameObject path;
+        public GameObject pathParent;
 
-        isRotating = false;
-        toggleState = true;
+        [SerializeField]
+        private float moveSpeed;
+        [SerializeField]
+        private float rotationSpeed = 30f;
 
-        toggleReference.action.started += TogglePathMesh;
-        rotateReference.action.started += RotatePath;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(pointsIndex <= Points.Length - 1)
+        private Transform mainCameraTransform;
+        private int pointsIndex;
+        private bool toggleState = true;
+        private bool isRotating = false;
+        // Start is called before the first frame update
+        void Start()
         {
-            transform.position = Vector3.MoveTowards(transform.position, Points[pointsIndex].transform.position, moveSpeed * Time.deltaTime);
 
-            if (transform.position == Points[pointsIndex].transform.position)
+            pointsIndex = 0;
+            transform.position = Points[pointsIndex].transform.position;
+
+            isRotating = false;
+            toggleState = true;
+            mainCameraTransform = GameObject.Find("XR Origin (XR Rig)/Camera Offset/Main Camera").transform;
+            if (!mainCameraTransform)
             {
-                pointsIndex++;
+                Debug.Log("No camera found");
+            }
+            toggleReference.action.started += TogglePathMesh;
+            rotateReference.action.started += RotatePath;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (pointsIndex <= Points.Length - 1)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, Points[pointsIndex].transform.position, moveSpeed * Time.deltaTime);
+
+                if (transform.position == Points[pointsIndex].transform.position)
+                {
+                    pointsIndex++;
+                }
+
+                if (pointsIndex == Points.Length)
+                {
+                    pointsIndex = 0;
+                }
             }
 
-            if (pointsIndex == Points.Length)
+            if (isRotating)
             {
-                pointsIndex = 0;
+                float rotationAmount = rotationSpeed * Time.deltaTime;
+                pathParent.transform.Rotate(Vector3.up, rotationAmount);
+            }
+
+            Debug.Log(Vector3.Distance(mainCameraTransform.position, this.transform.position));
+        }
+        private void OnDestroy()
+        {
+            toggleReference.action.started -= TogglePathMesh;
+            rotateReference.action.performed -= RotatePath;
+        }
+
+        private void TogglePathMesh(InputAction.CallbackContext context)
+        {
+            if (toggleState)
+            {
+                path.SetActive(false);
+                toggleState = !toggleState;
+            }
+            else
+            {
+                path.SetActive(true);
+                toggleState = !toggleState;
             }
         }
 
-        if (isRotating)
+        private void RotatePath(InputAction.CallbackContext context)
         {
-            float rotationAmount = rotationSpeed * Time.deltaTime;
-            pathParent.transform.Rotate(Vector3.up, rotationAmount);
+            isRotating = !isRotating;
+
         }
-
-    }
-    private void OnDestroy()
-    {
-        toggleReference.action.started -= TogglePathMesh;
-        rotateReference.action.performed -= RotatePath;
-    }
-
-    private void TogglePathMesh(InputAction.CallbackContext context)
-    {
-        if (toggleState)
-        {
-            path.SetActive(false);
-            toggleState = !toggleState;
-        }
-        else
-        {
-            path.SetActive(true);
-            toggleState = !toggleState;
-        }
-    }
-
-    private void RotatePath(InputAction.CallbackContext context)
-    {
-        isRotating = !isRotating;
-
     }
 }
