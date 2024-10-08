c3be93d74ff4cf01e9e3798eea5f1bf3f181e690
LilG00s3 <ekimmel8@gatech.edu>
2024-10-08 13:45:17 -0400

    Fixed the lookat and values

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -31,7 +31,7 @@ namespace UnityEngine
         private string filePath;
         private string outputString;
         private GameObject mainCamera;
-        private float[] moveSpeed = { 1.579f, 1.817f, 1.649f, 1.466f, 1.237f, 1.209f, 1.306f, 1.728f, 1.277f, 1.335f, 1.581f, 1.741f, 1.459f, 1.361f, 1.763f, 1.822f, 1.402f, 1.853f, 1.457f, 1.864f, 1.245f, 1.758f, 1.082f, 1.843f, 1.691f };
+        private float[] moveSpeed = { 0.8759f, 1.8271f, 1.4777f, 1.6173f, 0.9857f, 1.9980f, 0.8831f, 1.5290f, 1.8528f, 0.9548f, 0.6855f, 1.3045f, 1.5628f, 0.5291f, 0.8537f, 1.9775f, 1.2766f, 1.3836f, 1.3689f, 1.9837f};
         private int speedIndex;
         private int pointsIndex;
         private float tDelt;
@@ -45,7 +45,7 @@ namespace UnityEngine
             if (File.Exists(filePath))
                 File.Delete(filePath);
             isMoving = false;
-            facing = this.transform.parent;
+            facing = this.transform;
             previousMovingState = isMoving;
             tDelt = 0;
             filePath = System.IO.Path.Combine(Application.persistentDataPath, System.DateTime.Now.ToString("HH-mm-ss") + ".csv");
@@ -72,8 +72,8 @@ namespace UnityEngine
         // Update is called once per frame
         void Update()
         {
-            this.transform.parent.LookAt(new Vector3(mainCamera.transform.position.x, facing.position.y, mainCamera.transform.position.z));
-            if (tDelt >= 3)
+            this.transform.LookAt(new Vector3(mainCamera.transform.position.x, facing.position.y, mainCamera.transform.position.z));
+            if (tDelt >= 2)
             {
                 print("speed Changed");
                 tDelt = 0;
@@ -83,6 +83,7 @@ namespace UnityEngine
             {
                 if (isMoving)
                 {
+                    tDelt += Time.deltaTime;
                     transform.position = Vector3.MoveTowards(transform.position, Points[pointsIndex].transform.position, moveSpeed[speedIndex] * Time.deltaTime);
                 }
 
@@ -141,7 +142,7 @@ namespace UnityEngine
                 File.WriteAllText(filePath, sb.ToString());
             else
                 File.AppendAllText(filePath, sb.ToString());
-            tDelt += Time.deltaTime;
+            
 
         }
         private void OnDestroy()
